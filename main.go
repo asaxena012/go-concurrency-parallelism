900e90aeff80e69e61fb388cf7117323f4d28184
Aditya Saxena <60032144+asaxena012@users.noreply.github.com>
2021-12-20 14:03:36 +0530

    added mutex to protect concurrent cache r/w

--- main.go
+++ main.go
@@ -16,28 +16,29 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
 	// Loop 1 to 10 random ids
 	for i:=0; i<10; i++ {
 		id := rnd.Intn(10) + 1
 
 		wg.Add(2)
 		// Look for book with id in cache
-		go func(id int, wg *sync.WaitGroup){
-			if b, ok := queryCache(id); ok {
+		go func(id int, wg *sync.WaitGroup, mu *sync.Mutex){
+			if b, ok := queryCache(id, mu); ok {
 				fmt.Println("from cache:")
 				b.Display()
 			}
 			wg.Done()
-		}(id, wg)
+		}(id, wg, mu)
 
 		// if not fetch from db	
-		go func(id int, wg *sync.WaitGroup){
-			if b, ok := queryDB(id); ok {
+		go func(id int, wg *sync.WaitGroup, mu *sync.Mutex){
+			if b, ok := queryDB(id, mu); ok {
 				fmt.Println("from DB:")
 				b.Display()
 			}
 			wg.Done()
-		}(id, wg)
+		}(id, wg, mu)
 
 		// fmt.Println("Book not found!")
 		// time.Sleep(150 * time.Millisecond)
@@ -48,21 +49,25 @@ func main() {
 
 // Fetch from cache func
 
-func queryCache(id int) (books.Book, bool) {
+func queryCache(id int, mu *sync.Mutex) (books.Book, bool) {
+	mu.Lock()
 	b, ok := cache[id]
+	mu.Unlock()
 	return b, ok
 }
 
 // Fetch from db func
 
-func queryDB(id int) (books.Book, bool) {
+func queryDB(id int, mu *sync.Mutex) (books.Book, bool) {
 
 	time.Sleep(100 * time.Millisecond)
 
 	for _, b := range books.BookDB {
 		if b.ID == id {
-			// cache[id] = b
-			return b, true
+			mu.Lock()
+			cache[id] = b
+			mu.Unlock()
+			return b, true 
 		}
 	}
 	
